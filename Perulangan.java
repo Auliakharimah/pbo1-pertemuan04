0f07b7d293120feaafa4f2dff6a64a36db08b59e
Auliakharimah <62272038+Auliakharimah@users.noreply.github.com>
2020-05-30 09:28:17 +0700

    Add files via upload

---
+++ Perulangan.java
@@ -0,0 +1,14 @@
+package tugas;
+
+public class Perulangan {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x>0){
+            System.out.println("Nilai ke-" + x);
+            x--;
+        }
+    }
+}
+
+
+
