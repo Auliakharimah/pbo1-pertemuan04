b7ca6a4f5c05f0c9dcc1f8a11732857adb0e91bf
Auliakharimah <62272038+Auliakharimah@users.noreply.github.com>
2020-05-30 12:35:24 +0700

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,20 @@
+package tugas;
+
+public class Array1 {
+    public class Array {
+        public static void main(String[] args) {
+            String mataKuliah = new String[4];
+            mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+            mataKuliah[1] = "Sistem Informasi Geografis";
+            mataKuliah[2] = "Pemrograman Visual 2";
+            mataKuliah[3] = "Perancangan Database";
+            mataKuliah[4] = "Jaringan Komputer 2";
+
+            System.out.println(mataKuliah[0]);
+            System.out.println(mataKuliah[1]);
+            System.out.println(mataKuliah[2]);
+            System.out.println(mataKuliah[3]);
+            System.out.println(mataKuliah[4]);
+        }
+    }
+}
